acec7da84ad06b0bad4b127bab104650b9ce038f
tothadi <tothadam.onp@gmail.com>
2021-08-25 15:29:30 +0200

    added 404 to get-one

--- backend-express/api/controllers/get-one.js
+++ backend-express/api/controllers/get-one.js
@@ -4,7 +4,10 @@ module.exports = (objRep) => {
         const Model = Models[req.params.model];
         Model.findOne({ _id: req.params.id }).exec((err, result) => {
             if (err) {
-               return res.status(500).json({ error: err.message });
+                return res.status(500).json({ error: err.message });
+            }
+            if (!result) {
+                return res.status(404).json({ message: `No document with the id: ${req.params.id} can be found.` });
             }
             res.locals.document = result;
             res.locals.Model = Model;
