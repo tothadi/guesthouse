05208bdc14f5f42393287974012b196d1f4bfdca
tothadi <tothadam.onp@gmail.com>
2022-01-12 13:06:00 +0100

    sort docs

--- backend/api/middlewares/get-all.js
+++ backend/api/middlewares/get-all.js
@@ -3,6 +3,9 @@ module.exports = () => {
         const Model = res.locals.models[req.params.model];
         try {
             const docs = await Model.find({});
+            if (Object.keys(Model.schema.paths).includes('order')) {
+                docs.sort((a,b)=> a-b);
+            }
             return res.json(docs);
         } catch (error) {
             return res.status(500).json(error.message);
